095d00fba8ba3c8760a651f54d8363daa9f0e323
skociuba <kociuba.sebastian@gmail.com>
2023-05-24 17:04:38 +0200

    test husky

--- src/pages/SubPages/SubRoutes.js
+++ src/pages/SubPages/SubRoutes.js
@@ -3,6 +3,8 @@ import {Link} from 'react-router-dom';
 import {Outlet} from 'react-router-dom';
 const SubRoutes = () => (
   <>
+    {' '}
+    SubRoutes
     <nav>
       <Link to="/sub-pages/landing">Landing</Link>
       <Link to="/sub-pages/secondPages">SecondPage</Link>
